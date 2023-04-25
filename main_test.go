d352efd5ab88f48d60210d815b543a1f75759be3
Björn Albers <bjoernalbers@gmail.com>
2023-04-25 10:07:39 +0200

    Add forgotten test file.

---
+++ main_test.go
@@ -0,0 +1,25 @@
+package main
+
+import "testing"
+
+func TestIsMissing(t *testing.T) {
+	tests := []struct {
+		path string
+		want bool
+	}{
+		{
+			"/Applications/DoesNotExist.app",
+			true,
+		},
+		{
+			"/System/Library/CoreServices/Finder.app",
+			false,
+		},
+	}
+	for _, test := range tests {
+		app := App{test.path}
+		if got := app.IsMissing(); got != test.want {
+			t.Errorf("IsMissing(%v) = %v, want: %v", test.path, got, test.want)
+		}
+	}
+}
