e9e944fc6ed2641f8e95072f5e2b87082eed118f
Björn Albers <bjoernalbers@gmail.com>
2023-04-25 10:06:58 +0200

    Display version of currently installed LibreOffice.

--- main.go
+++ main.go
@@ -4,6 +4,9 @@ package main
 import (
 	"fmt"
 	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
 )
 
 func main() {
@@ -11,6 +14,13 @@ func main() {
 	if app.IsMissing() {
 		fmt.Println("LibreOffice is missing. Installation required.")
 	}
+
+	version, err := app.version()
+	if err != nil {
+		fmt.Println("error:", err)
+		os.Exit(1)
+	}
+	fmt.Println("Installed version:", version)
 }
 
 type App struct {
@@ -23,4 +33,17 @@ func (a *App) IsMissing() bool {
 		return false
 	}
 	return true
+}
+
+// version returns the app's version
+func (a *App) version() (string, error) {
+	cmd := exec.Command("/usr/bin/defaults",
+		"read",
+		filepath.Join(a.Path, "Contents/Info.plist"),
+		"CFBundleShortVersionString")
+	stdout, err := cmd.Output()
+	if err != nil {
+		return "", err
+	}
+	return strings.TrimSpace(string(stdout)), nil
 }
