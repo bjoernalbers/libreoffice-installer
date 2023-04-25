6667c4179c2da74feb07f2b47c1efe76720945d9
Björn Albers <bjoernalbers@gmail.com>
2023-04-25 09:42:30 +0200

    Check if LibreOffice is missing.

--- main.go
+++ main.go
@@ -1,5 +1,26 @@
 // libreoffice-installer
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 func main() {
+	app := App{"/Applications/LibreOffice.app"}
+	if app.IsMissing() {
+		fmt.Println("LibreOffice is missing. Installation required.")
+	}
+}
+
+type App struct {
+	Path string
+}
+
+func (a *App) IsMissing() bool {
+	_, err := os.Stat(a.Path)
+	if err == nil {
+		return false
+	}
+	return true
 }
