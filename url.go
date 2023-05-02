3464363895b94b5590030d4b56c2c4f7293f68e8
Björn Albers <bjoernalbers@gmail.com>
2023-05-02 11:31:08 +0200

    Add URL.Checksum() method.

--- url.go
+++ url.go
@@ -7,7 +7,7 @@ type URL struct {
 	Arch    string
 }
 
-// DiskImage returns URL for the LibreOffice Disk Image.
+// DiskImage returns URL for the Disk Image.
 func (url URL) DiskImage() (string, error) {
 	var arch, filenameArch string
 	switch url.Arch {
@@ -22,4 +22,13 @@ func (url URL) DiskImage() (string, error) {
 	}
 	return fmt.Sprintf("https://download.documentfoundation.org/libreoffice/stable/%s/mac/%s/LibreOffice_%s_MacOS_%s.dmg",
 		url.Version, arch, url.Version, filenameArch), nil
+}
+
+// Checksum returns URL for the Disk Image's SHA-256 Checksum.
+func (url URL) Checksum() (string, error) {
+	diskImageURL, err := url.DiskImage()
+	if err != nil {
+		return "", err
+	}
+	return diskImageURL + ".sha256", nil
 }
--- url_test.go
+++ url_test.go
@@ -51,3 +51,53 @@ func TestDiskImageURL(t *testing.T) {
 		}
 	}
 }
+
+func TestChecksumURL(t *testing.T) {
+	tests := []struct {
+		version string
+		arch    string
+		want    string
+		wantErr bool
+	}{
+		{
+			"7.4.6",
+			"x86_64",
+			"https://download.documentfoundation.org/libreoffice/stable/7.4.6/mac/x86_64/LibreOffice_7.4.6_MacOS_x86-64.dmg.sha256",
+			false,
+		},
+		{
+			"7.4.6",
+			"arm64",
+			"https://download.documentfoundation.org/libreoffice/stable/7.4.6/mac/aarch64/LibreOffice_7.4.6_MacOS_aarch64.dmg.sha256",
+			false,
+		},
+		{
+			"7.5.2",
+			"x86_64",
+			"https://download.documentfoundation.org/libreoffice/stable/7.5.2/mac/x86_64/LibreOffice_7.5.2_MacOS_x86-64.dmg.sha256",
+			false,
+		},
+		{
+			"7.5.2",
+			"arm64",
+			"https://download.documentfoundation.org/libreoffice/stable/7.5.2/mac/aarch64/LibreOffice_7.5.2_MacOS_aarch64.dmg.sha256",
+			false,
+		},
+		{
+			"7.4.6",
+			"",
+			"",
+			true,
+		},
+	}
+	for _, test := range tests {
+		url := URL{test.version, test.arch}
+		got, err := url.Checksum()
+		if (err != nil) != test.wantErr {
+			t.Errorf("%#v.Checksum() err = %v, wantErr: %v", url, err, test.wantErr)
+		}
+		if got != test.want {
+			t.Errorf("%#v.Checksum():\ngot:\t%q\nwant:\t%q", url, got, test.want)
+		}
+	}
+}
