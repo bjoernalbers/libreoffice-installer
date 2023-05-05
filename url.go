0417f5ca8199b9f27c2fbd3a062baa4d3b68e2d3
Björn Albers <bjoernalbers@gmail.com>
2023-05-05 09:10:03 +0200

    Rename variables in URL generation code.

--- url.go
+++ url.go
@@ -9,19 +9,19 @@ type URL struct {
 
 // DiskImage returns the Disk Image URL.
 func (url URL) DiskImage() (string, error) {
-	var arch, filenameArch string
+	var dirArch, baseArch string
 	switch url.Arch {
 	case "arm64":
-		arch = "aarch64"
-		filenameArch = arch
+		dirArch = "aarch64"
+		baseArch = dirArch
 	case "amd64":
-		arch = "x86_64"
-		filenameArch = "x86-64"
+		dirArch = "x86_64"
+		baseArch = "x86-64"
 	default:
 		return "", fmt.Errorf("unsupported architecture: %q", url.Arch)
 	}
 	return fmt.Sprintf("https://download.documentfoundation.org/libreoffice/stable/%s/mac/%s/LibreOffice_%s_MacOS_%s.dmg",
-		url.Version, arch, url.Version, filenameArch), nil
+		url.Version, dirArch, url.Version, baseArch), nil
 }
 
 // Checksum returns the Disk Image's Checksum URL.
