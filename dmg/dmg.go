9eece6a833838f890981b9dca9fc6ac746bc6870
Björn Albers <bjoernalbers@gmail.com>
2023-05-09 13:34:43 +0200

    Attach disk image below /Volumes.

--- dmg/dmg.go
+++ dmg/dmg.go
@@ -5,26 +5,24 @@ import (
 	"bytes"
 	"encoding/xml"
 	"fmt"
-	"os"
 	"os/exec"
 	"strings"
 )
 
 // Attach attaches the named disk image and returns its mount point.
 func Attach(name string) (string, error) {
-	dir, err := os.MkdirTemp("/tmp", "")
+	cmd := exec.Command("hdiutil", "attach", name, "-nobrowse", "-plist")
+	stdout, err := cmd.Output()
 	if err != nil {
-		return "", fmt.Errorf("attach disk image: %v", err)
-	}
-	cmd := exec.Command("hdiutil", "attach", name, "-mountpoint", dir, "-nobrowse")
-	var stderr bytes.Buffer
-	cmd.Stderr = &stderr
-	err = cmd.Run()
-	if err != nil {
-		firstLine, _, _ := strings.Cut(stderr.String(), "\n")
+		stderr := err.(*exec.ExitError).Stderr
+		firstLine, _, _ := strings.Cut(string(stderr), "\n")
 		return "", fmt.Errorf("%s: %s", firstLine, name)
 	}
-	return dir, nil
+	mountpoint, err := mountpointFromPlist(stdout)
+	if err != nil {
+		return "", err
+	}
+	return mountpoint, nil
 }
 
 // Detach detaches a disk image by the named device or mountpoint.
