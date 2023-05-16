3c981e1b19825dc690237ed0942db696999a8aeb
Björn Albers <bjoernalbers@gmail.com>
2023-05-16 13:32:50 +0200

    Determine current LibreOffice version by download website.

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/bjoernalbers/libreoffice-installer
 
 go 1.20
 
-require github.com/hashicorp/go-version v1.6.0 // indirect
+require (
+	github.com/hashicorp/go-version v1.6.0 // indirect
+	golang.org/x/net v0.10.0 // indirect
+)
--- main.go
+++ main.go
@@ -20,11 +20,10 @@ import (
 
 	"github.com/bjoernalbers/libreoffice-installer/dmg"
 	"github.com/hashicorp/go-version"
+	"golang.org/x/net/html"
 )
 
-const (
-	LibreOfficeVersion = "7.4.6"
-)
+const LibreOfficeDownloadURL = "https://update.libreoffice.org/description"
 
 func init() {
 	log.SetFlags(0)
@@ -32,6 +31,10 @@ func init() {
 }
 
 func main() {
+	LibreOfficeVersion, err := latestVersion(LibreOfficeDownloadURL)
+	if err != nil {
+		log.Fatal(err)
+	}
 	volume := os.Args[3]
 	app := App{filepath.Join(volume, "/Applications/LibreOffice.app")}
 	if !needsInstallation(app, LibreOfficeVersion) {
@@ -89,6 +92,61 @@ func main() {
 		log.Fatal("Copy of LibreOffice failed: ", err)
 	}
 	log.Print("Installation completed successfully")
+}
+
+// latestVersion returns the lastest available version of LibreOffice.
+func latestVersion(url string) (string, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		return "", fmt.Errorf("%s: %s", url, resp.Status)
+	}
+	version, err := parseVersion(resp.Body)
+	if err != nil {
+		return "", err
+	}
+	return version, nil
+}
+
+// parseVersion parsed the version from reader
+func parseVersion(r io.Reader) (string, error) {
+	z := html.NewTokenizer(r)
+	var foundVersionNumber bool
+	var versions []string
+	for {
+		tt := z.Next()
+		switch tt {
+		case html.ErrorToken:
+			if err := z.Err(); err != io.EOF {
+				return "", err
+			}
+			if len(versions) != 2 {
+				return "", fmt.Errorf("expected two version numbers: %v", versions)
+			}
+			return versions[1], nil
+		case html.StartTagToken:
+			t := z.Token()
+			if t.Data != "span" {
+				continue
+			}
+			for _, a := range t.Attr {
+				if a.Key == "class" && a.Val == "dl_version_number" {
+					foundVersionNumber = true
+				}
+			}
+		case html.TextToken:
+			if !foundVersionNumber {
+				continue
+			}
+			t := z.Token()
+			versions = append(versions, t.Data)
+		case html.EndTagToken:
+			foundVersionNumber = false
+		}
+	}
 }
 
 // needsInstallation returns true if installation of LibreOffice is required.
